248403482819e4e2391d9ecfbd87466c3654b423
Justin Lohner <lohner4@gmail.com>
2020-05-14 12:47:53 -0600

    Got the shopping cart and navigation components working with context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+
 import { ProductContext } from './contexts/ProductContext.js';
+import { CartContext } from './contexts/CartContext.js';
+
 import data from './data';
 
 // Components
@@ -20,16 +23,18 @@ function App() {
 	return (
 		<div className='App'>
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path='/'>
-					<Products />
-				</Route>
+					{/* Routes */}
+					<Route exact path='/'>
+						<Products />
+					</Route>
 
-				<Route path='/cart'>
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path='/cart'>
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
